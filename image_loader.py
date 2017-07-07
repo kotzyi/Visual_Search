3d7e6fbaecd27505fa5e18ecbe3e3e64c7e8345f
jlee <junyeop.lee@kt.com>
2017-07-07 11:23:58 +0900

    Add image loader

---
+++ image_loader.py
@@ -0,0 +1,56 @@
+import torch.utils.data as data
+
+from PIL import Image
+from PIL import ImageFile
+
+import os
+import json
+import re
+from glob import glob
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+IMG_EXTENSION = [
+	'.jpg', '.JPG', '.jpeg', '.JPEG',
+	'.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+]
+
+
+def is_imagE_file(filename):
+	return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+def make_dataset(path):
+	imgage = []
+	file_list = glob(path+'/**/*')
+
+	for item in file_list:
+		target = re.split(r'/',item)[-2]
+		images.append((item,int(target)))
+		
+	return images
+
+def default_loader(path):
+	return Image.open(path).convert('RGB')
+
+class ImageFolder(data.Dataset):
+	def __init__(self, root, transform = None, target_transform = None, loader = default_loader):
+		imgs = make_dataset(root)
+
+		self.root = root
+		self.imgs = imgs
+		self.transform = transform
+		self.target_transform = target_transform
+		self.loader = loader
+		self.train = train
+
+	def __getitem__(self, index):
+		path = self.imgs[index]
+		img = self.loader(path)
+
+		if self.transform is not None:
+			img = self.transform(img)
+
+		return img
+
+	def __len__(self):
+		return len(self.imgs)
